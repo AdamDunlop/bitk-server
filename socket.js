8198f28eeebae23d01ae4c1ebaa74f0d8820e65c
Adam Dunlop <adamblairdunlop@gmail.com>
2026-06-17 11:01:47 -0700

    Update scripts, assets, socket, and config (clean state)

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://192.168.1.96:3001", {
+const socket = io("http://10.0.0.77:3001", {
   transports: ["websocket"],
   reconnection: true,
 });
